8093672c6cd66f372bec3f7b06beb87879c556ab
Aljaž Sajko <aljaz.sajko@gmail.com>
2025-03-22 17:50:02 +0100

    6th commit

--- github 4 in a row/4_in_row.py
+++ github 4 in a row/4_in_row.py
@@ -6,8 +6,8 @@ from main import width, height, red, green, black, white,yellow
 
 pygame.init()
 blue = (0,0,255)
-stolpci = 6  #stolpci
-vrstice = 7 #vrstice
+stolpci = 7  #stolpci
+vrstice = 6 #vrstice
 velikost_kvadrata = 100
 
 
@@ -15,9 +15,9 @@ class Code_1v1:
     def __init__(self):
         pygame.display.set_caption("Connect 4:  1v1")
         self.screen = pygame.display.set_mode((width,height))
-        #self.screen.fill(white)
+        self.screen.fill(white)
         self.board = np.zeros((vrstice,stolpci))
-        #self.board= [[0]* vrstice for i in range(stolpci)]
+        #self.board= [[0]* stolpci for i in range(vrstice)]
         self.game_over = False
         self.turn = 0 # red = 1, yellow = 0
     def drop_piece(self,row,col,piece):
@@ -25,18 +25,19 @@ class Code_1v1:
     
     def is_valid_location(self,stolpec):
         return self.board[vrstice -1][stolpec] == 0
+        
 
     def naslednja_prosta_vrstica(self,stolpec):
         for i in range(vrstice):
             if self.board[i][stolpec] == 0:
                 return i
-
+        
     def draw_board(self):
         #plošča na kateri se igra
-        for vrstica in range(vrstice):
-            for stolpec in range(1,stolpci +1):
-                    pygame.draw.rect(self.screen,blue,( * velikost_kvadrata,stolpec * velikost_kvadrata,velikost_kvadrata,velikost_kvadrata))
-                    pygame.draw.circle(self.screen,white,(int(vrstica *  velikost_kvadrata + velikost_kvadrata/2), int(stolpec * velikost_kvadrata  + velikost_kvadrata/2)), int(velikost_kvadrata/2 - 5))
+        for stolpec in range(stolpci):
+            for vrstica in range(1,vrstice +1):
+                    pygame.draw.rect(self.screen,blue,(stolpec * velikost_kvadrata,vrstica * velikost_kvadrata,velikost_kvadrata,velikost_kvadrata))
+                    pygame.draw.circle(self.screen,white,(int(stolpec *  velikost_kvadrata + velikost_kvadrata/2), int(vrstica * velikost_kvadrata  + velikost_kvadrata/2)), int(velikost_kvadrata/2 - 5))
 
     def winig_move(self,piece):
            
@@ -80,14 +81,14 @@ class Code_1v1:
                     pygame.draw.rect(self.screen,white,(0,0,width,velikost_kvadrata))
                     pygame.draw.circle(self.screen,red if self.turn == 0 else yellow,(event.pos[0],int(velikost_kvadrata/2)),int(velikost_kvadrata/2 - 5))
                     pygame.display.update()
-                if event.type == pygame.MOUSEBUTTONDOWN:
+                if  event.type == pygame.MOUSEBUTTONDOWN:
                     pygame.draw.rect(self.screen,white,(0,0,width,velikost_kvadrata))
                     posx = event.pos[0]
-                    stolpec = posx// velikost_kvadrata
-                    if self.is_valid_location(stolpec):
+                    stolpec = min(posx // velikost_kvadrata, stolpci - 1) 
+                    if 0 <= stolpec < stolpci and self.is_valid_location(stolpec):
                         vrstica = self.naslednja_prosta_vrstica(stolpec)
                         self.drop_piece(vrstica,stolpec,1 if self.turn == 0 else 2)
-                        pygame.draw.circle(self.screen,red if self.turn == 0 else yellow,(stolpec * velikost_kvadrata + int(velikost_kvadrata/2), (vrstica + 1) * velikost_kvadrata + int(velikost_kvadrata/2)),int(velikost_kvadrata/2 - 5))
+                        pygame.draw.circle(self.screen,red if self.turn == 0 else yellow,(stolpec * velikost_kvadrata + int(velikost_kvadrata/2),(vrstice - vrstica - 1) * velikost_kvadrata + int(velikost_kvadrata/2)),int(velikost_kvadrata/2 - 5))
 
 
                         if self.winig_move(1 if self.turn == 0 else 2):
@@ -97,6 +98,7 @@ class Code_1v1:
                             else:
                                 label = font.render("Player 2 wins!!", 1, yellow)
                             self.screen.blit(label, (40,10))
+                            pygame.time.wait(10000)
                             self.game_over = True
                     self.turn += 1
                     self.turn = self.turn % 2
